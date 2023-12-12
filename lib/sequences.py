b4cc03af050fb2de89e95bcec932ac31ea62c5b4
joy Kurui <kurui.chepngetich@gmail.com>
2023-12-12 11:23:53 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < length :
+        num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(num)
+    print(fib_sequence[:length])
+
+print_fibonacci(9)
